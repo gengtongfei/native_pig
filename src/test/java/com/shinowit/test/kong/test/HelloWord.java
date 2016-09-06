03b205e5a57d47d33d77c4a22ee0c5d146af1a8d
KongXiangpeng <310960254@qq.com>
2016-09-06 10:55:14 +0800

    New changelist

---
+++ src/test/java/com/shinowit/test/kong/test/HelloWord.java
@@ -0,0 +1,10 @@
+package com.shinowit.test.kong.test;
+
+/**
+ * Created by gengtongfei on 2016/9/6.
+ */
+public class HelloWord {
+    public static void main(String[] args){
+        System.out.println("Hello Word!!");
+    }
+}
